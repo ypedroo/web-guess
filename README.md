7d297fdbb118ba9b652b5e4f824c001901cdbe9d
Ynoa Pedro <ynoa.pedro@outlook.com>
2020-06-19 19:38:26 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Deep Learning Web Guess
+
+## How to Run
+First export the main project as an app Flask
+On Bash use:
+``
+export FLASK_APP=main.py
+``
+On PowerShell use:
+``
+set FLASK_APP = main.py 
+``
+Then execute Flask
+``
+flask run
+``
+
+## Generate Another Train data
+You can use your own dataset, on scripts folder you find a jupyter notebook containing the Deep Learning train algorithm, feel free to generate your own models.
---
+++ main.py
@@ -0,0 +1,79 @@
+from base64 import b64encode
+from io import BytesIO
+
+import numpy as np
+from PIL import Image
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileRequired, FileAllowed
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing import image
+from wtforms import SubmitField
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'any secret key'
+
+bootstrap = Bootstrap(app)
+
+saved_model = load_model("Models/train_data.h5")
+saved_model._make_predict_function()
+
+
+class UploadForm(FlaskForm):
+    photo = FileField('Upload an image', validators=[FileAllowed(
+        ['jpg', 'png', 'jpeg'], u'Image only!'), FileRequired(u'File was empty!')])
+    submit = SubmitField(u'Predict')
+
+
+def preprocess(img):
+    width, height = img.shape[0], img.shape[1]
+    img = image.array_to_img(img, scale=False)
+
+    desired_width, desired_height = 150, 150
+
+    if width < desired_width:
+        desired_width = width
+    start_x = np.maximum(0, int((width - desired_width) / 2))
+
+    img = img.crop((start_x, np.maximum(0, height - desired_height),
+                    start_x + desired_width, height))
+    img = img.resize((150, 150))
+
+    img = image.img_to_array(img)
+    return img / 255.
+
+
+@app.route('/', methods=['GET', 'POST'])
+def predict():
+    form = UploadForm()
+    if form.validate_on_submit():
+        print(form.photo.data)
+        image_stream = form.photo.data.stream
+        original_img = Image.open(image_stream)
+        img = image.img_to_array(original_img)
+        img = preprocess(img)
+        img = np.expand_dims(img, axis=0)
+        prediction = saved_model.predict_classes(img)
+
+        if prediction[0] == 0:
+            result = "CAT"
+            print(prediction[0][0])
+        elif prediction[0] == 1:
+            result = "DOG"
+            print(prediction[0][0])
+        else:
+            result = "NOT AN ANIMAL"
+            print(prediction[0][0])
+
+        byte_io = BytesIO()
+        original_img.save(byte_io, format=original_img.format)
+        byte_arr = byte_io.getvalue()
+        encoded = b64encode(byte_arr)
+
+        return render_template('result.html', result=result, encoded_photo=encoded.decode('ascii'))
+    return render_template('index.html', form=form)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
